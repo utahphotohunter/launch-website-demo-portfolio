796a104b73ee7ec45884dbbde3a4d54198d7097b
utahphotohunter <mitch.hunter1197@gmail.com>
2024-11-03 01:51:10 -0600

    updated website to be more in line with best practices

--- script/script.js
+++ script/script.js
@@ -1,8 +1,16 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const toggleButton = document.querySelector('.mobile-menu-toggle');
-    const mobileMenu = document.querySelector('.mobile-menu-list-items');
+// mobile menu toggle
+const toggleButton = document.querySelector('.mobile-menu-toggle');
+const mobileMenu = document.querySelector('.mobile-menu-list-items');
 
-    toggleButton.addEventListener('click', function () {
-        mobileMenu.classList.toggle('active');
-    });
+toggleButton.addEventListener('click', function () {
+    mobileMenu.classList.toggle('active');
 });
+
+
+// footer copyright year
+
+const yearSpan = document.querySelector("#year");
+let date = document.lastModified;
+const editDate = new Date(date);
+let year = editDate.getFullYear();
+yearSpan.textContent = year;
